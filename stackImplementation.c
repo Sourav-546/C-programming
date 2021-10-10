64a70a8945b0e87b17200983c7cf5ede76ea5f24
ABHIRUP BHATTACHARYA <90793692+AbhirupB@users.noreply.github.com>
2021-10-10 13:19:33 +0530

    Create stackImplementation.c
    This code implements stack operations using C.
    Do review it.
    thank you.

---
+++ stackImplementation.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+
+int stack[100],choice,n,top,x,i;
+
+//function declaration for all the stack operations
+void push(void);
+void pop(void);
+void display(void);
+
+int main()
+{
+    top=-1;
+    printf("\n Enter the size of STACK[MAX=100]:");
+    scanf("%d",&n);
+
+    printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.DISPLAY\n\t 4.EXIT");   //options available to perform
+
+    do
+    {
+        printf("\n Enter the Choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            {
+                push();
+                break;
+            }
+            case 2:
+            {
+                pop();
+                break;
+            }
+            case 3:
+            {
+                display();
+                break;
+            }
+            case 4:
+            {
+                printf("\n\t Exit program!! ");
+                break;
+            }
+            default:
+            {
+                printf ("\n\t Enter a Valid Choice(1/2/3/4)");
+            }
+
+        }
+    }
+    while(choice!=4);
+    return 0;
+}
+
+//function definition for push operation
+void push()
+{
+    if(top>=n-1)
+    {
+        printf("\n\tSTACK overflow");
+
+    }
+    else
+    {
+        printf(" Enter a value to be pushed:");
+        scanf("%d",&x);
+        top++;
+        stack[top]=x;
+    }
+}
+
+//function definition for pop operation
+void pop()
+{
+    if(top<=-1)
+    {
+        printf("\n\t Stack underflow");
+    }
+    else
+    {
+        printf("\n\t The popped elements is %d",stack[top]);
+        top--;
+    }
+}
+
+//function definition to diplay updated stack
+void display()
+{
+    if(top>=0)
+    {
+        printf("\n The elements in STACK \n");
+        for(i=top; i>=0; i--)
+            printf("\n%d",stack[i]);
+        printf("\n Next Choice:");
+    }
+    else
+    {
+        printf("\n The STACK is empty");
+    }
+
+}
