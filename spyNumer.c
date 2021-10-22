8e3e4af1316e5df2e0c854c647b26f1dc96a1710
Satyam Kumar Dubey <56732302+Satyam308@users.noreply.github.com>
2021-10-22 22:26:00 +0530

    Create spyNumer.c

---
+++ spyNumer.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+  int n,sum=0,prod=1;
+  int d;
+  printf("Enter the number:");
+  scanf("%d",&n);
+  while(n>0{
+    d=n%10;
+    sum=sum+d;
+    prod=prod*d;
+    n=n/10;
+  }
+if(sum==prod){
+  printf("Given number is a spy number.");
+}
+else{
+  printf("Given number is not a spy number.");
+}
+return 0;
+        }
